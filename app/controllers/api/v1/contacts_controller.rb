d016f8a705dec189e8a4d15250408aa98d2a7e6a
kiboma <omayioben@gmail.com>
2022-11-25 00:24:15 +0300

    Add create in the controller

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -4,4 +4,16 @@ class Api::V1::ContactsController < ApplicationController
 
         render json: @contacts, status: :ok
     end
+
+    def create
+        @contact = Contact.new(contact_params)
+        @contact.save
+        render json: @contact, status: :created
+    end
+
+    private
+
+    def contact_params
+        params.require(:contact).permit(:first_name, :last_name, :email)
+    end
 end
