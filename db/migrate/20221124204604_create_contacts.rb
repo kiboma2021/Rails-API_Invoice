f94f09bda997bdf878bf1caed67cba16e0cf388f
kiboma <omayioben@gmail.com>
2022-11-25 00:01:30 +0300

    Create routes

--- db/migrate/20221124204604_create_contacts.rb
+++ db/migrate/20221124204604_create_contacts.rb
@@ -1,6 +1,9 @@
 class CreateContacts < ActiveRecord::Migration[7.0]
   def change
     create_table :contacts do |t|
+      t.string :first_name,
+      t.string :last_name,
+      t.email :omayioben@mail.com
 
       t.timestamps
     end
